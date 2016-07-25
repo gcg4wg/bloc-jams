cc851600c948f526ffb66bcba0dc1b9dbb3d73bf
Grant <gcg4wg@virginia.edu>
2016-07-25 10:48:03 -0400

    Add scroll event listener to selling points element

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,4 +1,4 @@
-var animateHero = function() {
+function animateHero() {
     var title = document.getElementsByClassName('hero-title');
     
     var fadeHero = function() {
@@ -7,8 +7,9 @@ var animateHero = function() {
     fadeHero();
 };
 
-var animatePoints = function() {
-    var points = document.getElementsByClassName('point');
+var pointsArray = document.getElementsByClassName('point');
+
+function animatePoints(points) {
   
     var revealPoint = function(i) {
         points[i].style.opacity = 1;
@@ -21,3 +22,20 @@ var animatePoints = function() {
         revealPoint(i);
     };
 };
+
+ window.onload = function() {
+     // automatically animate selling points on tall enough screens
+     if (window.innerHeight > 950) {
+         animatePoints(pointsArray);
+     }
+     // grab selling points
+    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+     // calculate the distance user must scroll to animate selling points 
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+    // 
+     window.addEventListener('scroll', function(event) {
+        if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+            animatePoints(pointsArray);
+        }
+     });
+ }
