63880907c4411f462c800650cb3fa8d73bac34aa
Grant <gcg4wg@virginia.edu>
2016-08-02 11:44:40 -0400

    add jquery to landing page

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,40 +1,32 @@
-function animateHero() {
-    var title = document.getElementsByClassName('hero-title');
-    
-    var fadeHero = function() {
-        title[0].style.opacity = 1;
-    };
-    fadeHero();
+var animateHero = function() {    
+    $('.hero-title').css({
+        opacity: 1,
+    });
 };
 
-var pointsArray = document.getElementsByClassName('point');
-
-var revealPoint = function(point) {
-        point.style.opacity = 1;
-        point.style.transform = "scaleX(1) translateY(0)";
-        point.style.msTransform = "scaleX(1) translateY(0)";
-        point.style.WebkitTransform = "scaleX(1) translateY(0)";
+var animatePoints = function() {
+    var revealPoint = function() {
+       $(this).css({
+           opacity: 1,
+           transform: 'scaleX(1) translateY(0)'
+       });
+    };
+    $.each($('.point'), revealPoint);
 };
       
-function animatePoints(points) {
-    forEach(points, revealPoint);
-};
-
- window.onload = function() {
-     // animate hero title
+ $(window).load(function() {
+     // first animate hero title
      animateHero();
      // automatically animate selling points on tall enough screens
-     if (window.innerHeight > 950) {
-         animatePoints(pointsArray);
+     if ($(window).height() > 950) {
+         animatePoints;
      }
-     // grab selling points
-    var sellingPoints = document.getElementsByClassName('selling-points')[0];
      // calculate the distance user must scroll to animate selling points 
-    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
-    // 
-     window.addEventListener('scroll', function(event) {
-        if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
-            animatePoints(pointsArray);
-        }
+     var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
+    // execute selling points animation on scroll
+     $(window).scroll(function(event) {
+         if ($(window).scrollTop() >= scrollDistance) {
+             animatePoints();
+         }
      });
- }
+ });
